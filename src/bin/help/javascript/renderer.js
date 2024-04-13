6937e9c5aa39492148ad5fbd222a110fbf5b0b24
Hollo <127456155+DevHollo@users.noreply.github.com>
2024-04-13 16:24:39 -0600

    Delete src directory

--- src/bin/help/javascript/renderer.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('codeSourceBtn').addEventListener('click', () => {
-    require('electron').ipcRenderer.send('open-url', 'https://github.com/DevHollo/hmod-url-protocol/tree/main/src');
-});
-
-document.getElementById('allProtocolsBtn').addEventListener('click', () => {
-    require('electron').ipcRenderer.send('open-url', 'https://raw.githubusercontent.com/DevHollo/hmod-url-protocol/main/protocols.txt');
-});
